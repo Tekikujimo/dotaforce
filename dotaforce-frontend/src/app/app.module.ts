5233bff2902c20052c2334550cd20be3e4de31c7
“Diego” <diegocavero2035@gmail.com>
2020-03-22 16:29:00 +0100

    Se arreglo ligeramente la visualizacion del navbar y se añadio configuracion para prevenir el error de cross origin domain.

--- dotaforce-frontend/src/app/app.module.ts
+++ dotaforce-frontend/src/app/app.module.ts
@@ -1,19 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule,HttpClientJsonpModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './commons/header/navbar/navbar.component';
+import { BodyContentComponent } from './commons/body-content/body-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    BodyContentComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    HttpClientJsonpModule,
     AppRoutingModule
   ],
   providers: [],
