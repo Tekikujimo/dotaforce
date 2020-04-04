7cec4d1c1b019a2b27c7e507ea5b418c55257d4c
“Diego” <diegocavero2035@gmail.com>
2020-04-04 19:46:17 +0200

    Se añadio componente de registro de usuario,resultado y jugador

--- dotaforce-frontend/src/app/actions/action/action.component.ts
+++ dotaforce-frontend/src/app/actions/action/action.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-action',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActionComponent implements OnInit {
 
-  constructor() { }
+  accion:string;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.accion = params['action'];
+   });
   }
 
 }
