07c9d2816b2c133a3c1441003e7d272f70fe1c8a
Tekikujimo <diegofenix2012@gmail.com>
2023-10-11 14:20:52 +0200

    Se carga datos de Heroes para no insertarlos manualmente, a futuro se añadiran mas registros

---
+++ dotaforce-backend/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO HEROES(NOMBRE, INFORMACION_ADICIONAL) VALUES('SVEN','Sven es el hijo bastardo de un Caballero de la Vigilia, nacido de un Merante Pálido y criado en las Ruinas de la Costa Sombría.');
+INSERT INTO HEROES(NOMBRE, INFORMACION_ADICIONAL) VALUES('LINA','La esencia de Lina le permite concentrar la energía del sol y hacer que el aire prenda a su voluntad.');
+INSERT INTO HEROES(NOMBRE, INFORMACION_ADICIONAL) VALUES('CRYSTAL MAIDEN','Sven es el hijo bastardo de un Caballero de la Vigilia, nacido de un Merante Pálido y criado en las Ruinas de la Costa Sombría.');
+INSERT INTO HEROES(NOMBRE, INFORMACION_ADICIONAL) VALUES('SNIPER','Crystal Maiden es un personaje de Agilidad a larga distancia, apoya a sus compañeros regenerando maná cuando tengan poca vida para que puedan ser eliminados rápidamente.');
+INSERT INTO HEROES(NOMBRE, INFORMACION_ADICIONAL) VALUES('MAGINA','Anti-Mage es un héroe de agilidad, atributo que le da armadura y velocidad en sus ataques cuerpo a cuerpo');
